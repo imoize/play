d73e894cdbbaf7c1bea6bbfd190586f10a407dc5
imoize <51510865+imoize@users.noreply.github.com>
2023-03-04 07:00:34 +0700

    Add sript.sh

---
+++ patches/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod +x /mkimage-alpine.bash && \
+/mkimage-alpine.bash && \
+mkdir /build-out && \
+tar xf /rootfs.tar.xz -C /build-out && \
+sed -i -e 's/^root::/root:!:/' //build-out/etc/shadow
